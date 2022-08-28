cc5e37a59a4334edd1f4898b03eb4e3d5e6a2162
LoadXBare <loadxbare@gmail.com>
2022-08-28 11:38:50 +0100

    [1.7.2] Updated rank card visuals using Canvas
    Additional Changes:
    • Added undici for better performance of HTTP requests

--- src/commands/ranking/rank.ts
+++ src/commands/ranking/rank.ts
@@ -1,11 +1,11 @@
-import { EmbedBuilder, GuildMember, inlineCode } from 'discord.js';
+import * as Canvas from '@napi-rs/canvas';
+import { AttachmentBuilder, GuildMember, inlineCode, quote } from 'discord.js';
+import { request } from 'undici';
 import { fetchMemberRanking } from '../../api/guild-ranking.js';
-import { COLORS } from '../../config/constants.js';
 import { BotCommand, Command, GuildRanking, XPLevelBounds } from '../../index.js';
 import { fetchMemberLeaderboardPosition } from '../../lib/guild-ranking/handler.js';
 import { fetchXPLevelBounds } from '../../lib/guild-ranking/xp-level.js';
 import { fetchGuildMember } from '../../lib/misc/fetch-guild-member.js';
-import { createProgressBar } from '../../lib/misc/progress-bar.js';
 import { sendError } from '../../lib/misc/send-error.js';
 
 const checkRankCommand = async (args: BotCommand): Promise<void> => {
@@ -36,17 +36,53 @@ const checkRankCommand = async (args: BotCommand): Promise<void> => {
 		memberLeaderboardPos = await fetchMemberLeaderboardPosition(member);
 	}
 
-	const memberRankEmbed = new EmbedBuilder()
-		.setThumbnail(member.displayAvatarURL())
-		.setTitle(`Viewing rank card for ${member.user.tag}`)
-		.setDescription(`**Level:** ${memberRanking.xpLevel}\
-			\n**Rank:** #${memberLeaderboardPos}\
-			\n**Total XP:** ${memberRanking.xp}\
-			\n\n**Progress:** ${memberRanking.xp - xpLevelBounds.lower} / ${xpLevelBounds.upper - xpLevelBounds.lower} XP\
-			\n${createProgressBar(memberRanking.xp - xpLevelBounds.lower, xpLevelBounds.upper - xpLevelBounds.lower, 10, true)}`)
-		.setColor(COLORS.COMMAND);
+	// Create canvas
+	const canvas = Canvas.createCanvas(823, 274);
+	Canvas.GlobalFonts.registerFromPath('./assets/Ubuntu-Medium.ttf', 'ubuntu-medium');
+	const ctx = canvas.getContext('2d');
 
-	message.reply({ embeds: [memberRankEmbed] });
+	// Draw background
+	const background = await Canvas.loadImage('./assets/rank_card_background.png');
+	ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+	// Draw member's profile picture
+	const { body } = await request(member.displayAvatarURL({ extension: 'png' }));
+	const avatar = await Canvas.loadImage(await body.arrayBuffer());
+	ctx.beginPath();
+	ctx.arc(136, 136, 110, 0, Math.PI * 2, true);
+	ctx.closePath();
+	ctx.save();
+	ctx.lineWidth = 8;
+	ctx.strokeStyle = '#FFFFFF';
+	ctx.stroke();
+	ctx.clip();
+	ctx.drawImage(avatar, 25, 25, 220, 220);
+	ctx.restore();
+
+	// Draw text
+	const progressPercent = Math.round(((memberRanking.xp - xpLevelBounds.lower) / (xpLevelBounds.upper - xpLevelBounds.lower)) * 100);
+	ctx.fillStyle = '#FFFFFF';
+	ctx.font = `${member.displayName.length > 16 ? 40 - ((member.displayName.length - 16) * 1.0625) : '40'}px ubuntu-medium`;
+	ctx.fillText(member.displayName, 270, 75);
+	ctx.font = '40px ubuntu-medium';
+	ctx.fillText(`#${memberLeaderboardPos}`, 700, 75);
+	ctx.font = '30px ubuntu-medium';
+	ctx.fillStyle = '#FFFFFF';
+	ctx.fillText(`${progressPercent}%`, 506, 200);
+	ctx.font = '20px ubuntu-medium';
+	ctx.fillText(`${memberRanking.xp - xpLevelBounds.lower} / ${xpLevelBounds.upper - xpLevelBounds.lower} XP`, 340, 155);
+	ctx.fillText(`Level ${memberRanking.xpLevel}`, 675 - 10 * memberRanking.xpLevel.toString().length, 155);
+	ctx.fillText(`Total XP${' '.repeat(66 - memberRanking.xp.toString().length * 2)}${memberRanking.xp}`, 340, 235);
+
+	// Draw progress bar
+	const progressBar = await Canvas.loadImage('./assets/rank-card-progress-bar.png');
+	ctx.fillStyle = '#00FF66';
+	ctx.fillRect(337, 172, progressPercent * 4, 33);
+	ctx.drawImage(progressBar, 0, 0, canvas.width, canvas.height);
+
+	const attachment = new AttachmentBuilder(await canvas.encode('png'), { name: 'rank-card.png' });
+
+	message.reply({ content: quote(`Viewing rank card • [ ${member.user.tag} ]`), files: [attachment] });
 };
 
 export const checkRank: Command = {
