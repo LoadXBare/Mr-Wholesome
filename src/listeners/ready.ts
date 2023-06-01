728a923e143587815337d3a696fbfce60191212f
LoadXBare <loadxbare@gmail.com>
2023-06-01 13:06:37 +0100

    🔧 Converted ready.ts to ReadyHandler class ➕ Added function to fetch all guild members on bot start-up

--- src/listeners/ready.ts
+++ src/listeners/ready.ts
@@ -1,7 +1,32 @@
-import { Events } from 'discord.js';
+import { Collection, Events, GuildMember } from 'discord.js';
 import client from '../index.js';
+import { EventHandler } from '../lib/config.js';
 import { Utils } from '../lib/utilities.js';
 
-client.once(Events.ClientReady, () => {
-  Utils.log(`Logged in as ${client.user?.tag}! [${client.user?.id}]`, true);
+class ReadyHandler extends EventHandler {
+  handle() {
+    this.#logClientReady();
+    this.#fetchAllGuildMembers();
+  }
+
+  async #logClientReady() {
+    Utils.log(`Logged in as ${client.user?.tag}! [${client.user?.id}]`, true);
+  }
+
+  // This is done so all guild members are immediately cached meaning events will be logged from them
+  async #fetchAllGuildMembers() {
+    const fetchedGuildMembers: Array<Promise<Collection<string, GuildMember>>> = [];
+
+    client.guilds.cache.forEach((guild) => {
+      if (guild.available) {
+        fetchedGuildMembers.push(guild.members.fetch());
+      }
+    });
+
+    await Promise.all(fetchedGuildMembers);
+  }
+}
+
+client.once(Events.ClientReady, async () => {
+  new ReadyHandler().handle();
 });
